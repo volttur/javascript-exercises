a152f08b0cdc7753bf7aec2a402a8ab7753e88ac
Aristov.A <volttur@yandex.ru>
2022-07-18 15:15:26 +0300

    Complete 5 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum < 0 || lastNum < 0) return "ERROR";
+    if (typeof(firstNum) !== "number" || typeof(lastNum) !== "number") return "ERROR";
+    let sum = 0;
+    let beginNum = firstNum < lastNum ? firstNum : lastNum;
+    let endNum = firstNum > lastNum ? firstNum : lastNum;
 
+    for (let i = beginNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
