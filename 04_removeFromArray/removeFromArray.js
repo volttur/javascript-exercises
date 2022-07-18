a87dd1e231de4ada5a05c312224a36d790585fb9
Aristov.A <volttur@yandex.ru>
2022-07-18 14:50:55 +0300

    Complete 4 task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        if (arr.indexOf(args[i]) < 0) {
+            continue;
+        } else {
+            arr.splice(arr.indexOf(args[i]), 1);
+        }   
+    }
+    return arr;
 };
 
 // Do not edit below this line
