71dfbbbce1d73592dd073564c85e34dc7ae810e6
Aristov.A <volttur@yandex.ru>
2022-07-18 16:58:10 +0300

    Complete 7 task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel;
+  cel = (far - 32) * 5 / 9
+  return Number.isInteger(cel) ? cel : Number(cel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far;
+  far = (cel * 9 / 5) + 32;
+  return Number.isInteger(far) ? far : Number(far.toFixed(1));
 };
 
 // Do not edit below this line
