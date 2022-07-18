5735cc4192f7594b7940555028c5725b3f680243
Aristov.A <volttur@yandex.ru>
2022-07-18 12:15:00 +0300

    Complete 1 and 2 tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = '';
+
+    if (num < 0) return "ERROR";
+
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+
+    return result;
 
 };
+
+/*
+function repeatString(str, num) {
+        for (let i = 0; i < num; i++) {
+            
+        } 
+   }
+*/ 
 
 // Do not edit below this line
 module.exports = repeatString;
